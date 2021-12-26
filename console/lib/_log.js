94531df3aae116fa6539dd20612137d182facaff
Criticalcarpet <76092002+Criticalcarpet@users.noreply.github.com>
2021-12-26 16:16:12 +0530

    🐛

--- console/lib/_log.js
+++ console/lib/_log.js
@@ -1,2 +1,4 @@
 
-export const LOGGER = () => { console.log("%c+" , "font-size: 1px; padding: 180px 320px;background: url('https://c.tenor.com/ejU6O27fjvsAAAAd/among-us-drip.gif');background-repeat: no-repeat") }
+export const LOGGER = () => { console.log("%c+", "font-size: 1px; padding: 180px 320px;background: url('https://c.tenor.com/ejU6O27fjvsAAAAd/among-us-drip.gif');background-repeat: no-repeat") 
+console.log('%c ', 'font-size: 1px; padding: 125px 125px; background-size: 250px 250px; background: no-repeat url(https://i2.wp.com/i.giphy.com/media/12BYUePgtn7sis/giphy-downsized.gif?w=770&amp;ssl=1);')
+}
